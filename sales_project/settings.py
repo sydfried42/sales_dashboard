dc078b6a8eb59bcf0d2d54fd1f4514613e2bc1b7
sydneyfriedman <friedman.sydneyl@gmail.com>
2025-01-10 16:00:06 -0500

    connected postgresql

--- sales_project/settings.py
+++ sales_project/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'sales_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'sales_dashboard_db', 
+        'USER': 'sales_user', 
+        'PASSWORD': '1234', 
+        'HOST': 'localhost', 
+        'PORT': '5432',  # Default PostgreSQL port
     }
 }
 
