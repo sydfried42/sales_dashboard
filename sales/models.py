8ce5377ee9be63b08fa69b5c375357e509c117bf
sydneyfriedman <friedman.sydneyl@gmail.com>
2025-01-10 16:13:24 -0500

    registered models in admin

--- sales/models.py
+++ sales/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
-# Create your models here.
+
+class Product(models.Model):
+    product_id = models.IntegerField(primary_key=True) 
+    name = models.CharField(max_length=255)
+    category = models.CharField(max_length=100)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def __str__(self):
+        return self.name
+
+class Order(models.Model):
+    order_id = models.IntegerField(unique=True)
+    customer_id = models.IntegerField()
+    status = models.CharField(max_length=50)
+    payment_method = models.CharField(max_length=50)
+    total_amount = models.DecimalField(max_digits=10, decimal_places=2)
+    order_date = models.DateField()
+
+    def __str__(self):
+        return f"Order {self.order_id} by Customer {self.customer_id}"
+
+class Payment(models.Model):
+    order = models.ForeignKey(Order, on_delete=models.CASCADE)
+    payment_method = models.CharField(max_length=50)
+    payment_date = models.DateField()
+    payment_amount = models.DecimalField(max_digits=10, decimal_places=2)
+    
+    def __str__(self):
+        return f"Payment for Order {self.order.order_id} - {self.payment_method}"
